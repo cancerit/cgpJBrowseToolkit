5c567bee04598d223124d553913045e05a5ef2c4
Keiran Raine <kr2@sanger.ac.uk>
2018-02-23 20:25:53 +0000

    Clean up after each image incase it's disk

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -26,31 +26,31 @@ cd $SCRIPT_PATH/..
 
 node --throw-deprecation js/jbrowse_rasterize.js --locs test/volvox.bed --navOff --baseUrl 'http://0.0.0.0:8080/?tracks=DNA%2CTranscript%2Cvolvox-sorted_bam_coverage%2Cvolvox-sorted_bam&data=sample_data%2Fjson%2Fvolvox'
 ehandle $?
-ls -l ctgA_17174-23150.png
+rm ctgA_17174-23150.png
 ehandle $?
 node --throw-deprecation js/jbrowse_rasterize.js --imgType jpeg --locs test/volvox.bed --navOff --baseUrl 'http://0.0.0.0:8080/?tracks=DNA%2CTranscript%2Cvolvox-sorted_bam_coverage%2Cvolvox-sorted_bam&data=sample_data%2Fjson%2Fvolvox'
 ehandle $?
-ls -l ctgA_17174-23150.jpeg
+rm ctgA_17174-23150.jpeg
 ehandle $?
 node --throw-deprecation js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff
 ehandle $?
-ls -l EmbeddedUrl/ctgA_17174-23150.png
+rm EmbeddedUrl/ctgA_17174-23150.png
 ehandle $?
 node --throw-deprecation js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff --imgType pdf
 ehandle $?
-ls -l EmbeddedUrl/ctgA_17174-23150.pdf
+rm EmbeddedUrl/ctgA_17174-23150.pdf
 ehandle $?
 node --throw-deprecation js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff --imgType pdf --zoom 2 --outdir z2
 ehandle $?
-ls -l z2/EmbeddedUrl/ctgA_17174-23150.pdf
+rm z2/EmbeddedUrl/ctgA_17174-23150.pdf
 ehandle $?
 node --throw-deprecation js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff --imgType png --zoom 2 --outdir z2
 ehandle $?
-ls -l z2/EmbeddedUrl/ctgA_17174-23150.png
+rm z2/EmbeddedUrl/ctgA_17174-23150.png
 ehandle $?
 node --throw-deprecation js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff --imgType png --outdir compact --dMode compact
 ehandle $?
-ls -l compact/EmbeddedUrl/ctgA_17174-23150.png
+rm compact/EmbeddedUrl/ctgA_17174-23150.png
 ehandle $?
 
 killserver
