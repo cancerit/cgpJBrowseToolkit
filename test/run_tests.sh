3cd1520326a03859b7aa8d01d1e85212905b8fd9
Keiran Raine <kr2@sanger.ac.uk>
2018-02-23 17:28:53 +0000

    Specify node options in test file

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -17,31 +17,31 @@ cd $SCRIPT_PATH/..
 
 EXIT_SUM=0
 
-js/jbrowse_rasterize.js --locs test/volvox.bed --navOff --baseUrl 'http://0.0.0.0:8080/?tracks=DNA%2CTranscript%2Cvolvox-sorted_bam_coverage%2Cvolvox-sorted_bam&data=sample_data%2Fjson%2Fvolvox'
+node --throw-deprecation js/jbrowse_rasterize.js --locs test/volvox.bed --navOff --baseUrl 'http://0.0.0.0:8080/?tracks=DNA%2CTranscript%2Cvolvox-sorted_bam_coverage%2Cvolvox-sorted_bam&data=sample_data%2Fjson%2Fvolvox'
 ehandle $?
 ls -l ctgA_17174-23150.png
 ehandle $?
-js/jbrowse_rasterize.js --imgType jpeg --locs test/volvox.bed --navOff --baseUrl 'http://0.0.0.0:8080/?tracks=DNA%2CTranscript%2Cvolvox-sorted_bam_coverage%2Cvolvox-sorted_bam&data=sample_data%2Fjson%2Fvolvox'
+node --throw-deprecation js/jbrowse_rasterize.js --imgType jpeg --locs test/volvox.bed --navOff --baseUrl 'http://0.0.0.0:8080/?tracks=DNA%2CTranscript%2Cvolvox-sorted_bam_coverage%2Cvolvox-sorted_bam&data=sample_data%2Fjson%2Fvolvox'
 ehandle $?
 ls -l ctgA_17174-23150.jpeg
 ehandle $?
-js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff
+node --throw-deprecation js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff
 ehandle $?
 ls -l EmbeddedUrl/ctgA_17174-23150.png
 ehandle $?
-js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff --imgType pdf
+node --throw-deprecation js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff --imgType pdf
 ehandle $?
 ls -l EmbeddedUrl/ctgA_17174-23150.pdf
 ehandle $?
-js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff --imgType pdf --zoom 2 --outdir z2
+node --throw-deprecation js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff --imgType pdf --zoom 2 --outdir z2
 ehandle $?
 ls -l z2/EmbeddedUrl/ctgA_17174-23150.pdf
 ehandle $?
-js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff --imgType png --zoom 2 --outdir z2
+node --throw-deprecation js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff --imgType png --zoom 2 --outdir z2
 ehandle $?
 ls -l z2/EmbeddedUrl/ctgA_17174-23150.png
 ehandle $?
-js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff --imgType png --outdir compact --dMode compact
+node --throw-deprecation js/jbrowse_rasterize.js --locs test/volvox_urlEmbedded.bed --navOff --imgType png --outdir compact --dMode compact
 ehandle $?
 ls -l compact/EmbeddedUrl/ctgA_17174-23150.png
 ehandle $?
